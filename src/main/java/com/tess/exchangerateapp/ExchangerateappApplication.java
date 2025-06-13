0e40ac01f5f982e32522faaf4bb63618b970e381
Tess Williams <85091036+tessrexx@users.noreply.github.com>
2025-06-13 23:12:16 +1200

    Initial commit

---
+++ src/main/java/com/tess/exchangerateapp/ExchangerateappApplication.java
@@ -0,0 +1,13 @@
+package com.tess.exchangerateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerateappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tess/exchangerateapp/ExchangerateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tess.exchangerateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
